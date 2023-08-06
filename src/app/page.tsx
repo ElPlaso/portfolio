ca93060bc4a5c133dd5f4c2020567d705a0dc8e7
ElPlaso <plahsoekusay@gmail.com>
2023-08-06 21:19:13 +1200

    update homepage padding layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Welcome from "@/components/welcome";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      <Welcome />
+    <main className="flex min-h-screen flex-col items-center justify-between p-16 max-sm:pt-0 max-sm:px-4">
+      <div className="flex flex-col space-y-8 w-[700px] mt-8 max-md:mt-2 max-md:w-[100%] items-start justify-start max-sm:px-1">
+        <Welcome />
+      </div>
     </main>
   );
 }
--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -10,46 +10,41 @@ import ReloadButton from "./reload-button";
 export default function Welcome() {
   return (
     <DetailsProvider>
-      <div className="flex flex-col space-y-8 w-[700px] mt-8 max-md:mt-2 max-md:w-[100%] items-start justify-start">
-        <a href="/cv.pdf" target="_blank">
-          <Image
-            src="/favicon.ico"
-            width="75"
-            height="75"
-            alt="icon"
-            className="hover:shadow-lg hover:ring-4 hover:ring-blue-100 rounded-sm"
-            title="Download CV"
-          />
-        </a>
-        <div className="space-y-4 text-gray-500 dark:text-gray-400">
-          <h1 className="text-gray-800 text-2xl font-medium dark:text-white">
-            Welcome to my website
-          </h1>
-          <h3>
-            <span className="font-bold">plasokusay</span> is able to connect.
-          </h3>
-          <div>
-            Try:
-            <ul className="list-disc ml-10">
-              <li>Checking my contact info</li>
-              <li>
-                <Link
-                  className="text-blue-500 hover:underline"
-                  href="/portfolio"
-                >
-                  Checking out my portfolio
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <p className="text-xs">SUCC_CONNECTION_ACCEPTED</p>
+      <a href="/cv.pdf" target="_blank">
+        <Image
+          src="/favicon.ico"
+          width="75"
+          height="75"
+          alt="icon"
+          className="hover:shadow-lg hover:ring-4 hover:ring-blue-100 rounded-sm"
+          title="Download CV"
+        />
+      </a>
+      <div className="space-y-4 text-gray-500 dark:text-gray-400">
+        <h1 className="text-gray-800 text-2xl font-medium dark:text-white">
+          Welcome to my website
+        </h1>
+        <h3>
+          <span className="font-bold">plasokusay</span> is able to connect.
+        </h3>
+        <div>
+          Try:
+          <ul className="list-disc ml-10">
+            <li>Checking my contact info</li>
+            <li>
+              <Link className="text-blue-500 hover:underline" href="/portfolio">
+                Checking out my portfolio
+              </Link>
+            </li>
+          </ul>
         </div>
-        <div className="w-full flex flex-row justify-between">
-          <DetailsButton />
-          <ReloadButton />
-        </div>
-        <Details />
+        <p className="text-xs">SUCC_CONNECTION_ACCEPTED</p>
       </div>
+      <div className="w-full flex flex-row justify-between">
+        <DetailsButton />
+        <ReloadButton />
+      </div>
+      <Details />
     </DetailsProvider>
   );
 }
