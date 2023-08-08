a7b8db43f150d61672e55c786e56176f9eff4633
ElPlaso <plahsoekusay@gmail.com>
2023-08-08 19:41:12 +1200

    add active menu motion.

--- src/components/layout/navbar-link.tsx
+++ src/components/layout/navbar-link.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import NavBarLink from "@/types/navbarlink";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -11,13 +12,19 @@ export default function NavBarLink({ navbarLink }: { navbarLink: NavBarLink }) {
     <Link
       href={navbarLink.href}
       className={
-        "inline-block p-4 border-b-2 rounded-t-lg " +
+        "inline-block p-4 relative " +
         (isActive
-          ? "text-blue-600 border-b-2 border-blue-600 dark:text-blue-500 dark:border-blue-500"
-          : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 active:text-blue-500 active:border-blue-500 dark:active:text-blue-400 dark:active:border-blue-400")
+          ? "text-blue-600  dark:text-blue-500 "
+          : "hover:text-gray-600  dark:hover:text-gray-300 active:text-blue-500  dark:active:text-blue-400")
       }
       aria-current="page"
     >
+      {isActive && (
+        <motion.span
+          layoutId="underline"
+          className="absolute left-0 bottom-[1px] block h-[2px] w-full rounded-sm bg-blue-600 dark:bg-blue-500"
+        />
+      )}
       {navbarLink.title}
     </Link>
   );
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -26,7 +26,7 @@ export default function NavBar() {
           />
         </svg>
       </Link>
-      <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+      <div className="text-sm font-medium text-center text-gray-500">
         <ul className="flex flex-wrap -mb-px space-x-2">
           {navbarLinks.map((navbarLink) => (
             <li key={navbarLink.title}>
