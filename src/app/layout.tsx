150794da8f851e204e92c9268d8e91bb7a74e4be
ElPlaso <plahsoekusay@gmail.com>
2023-08-12 16:43:53 +1200

    add vercel analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="max-sm:snap-y max-sm:snap-mandatory scroll-auto">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
