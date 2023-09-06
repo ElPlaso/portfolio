939b1517f05cff66a6a837cb4b55dac81212f2ce
kusaymoo <kusaymoo@ecs.vuw.ac.nz>
2023-09-06 23:08:27 +1200

    add sanity schemas for projects and experiences.

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,88 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { defineField, type SchemaTypeDefinition } from "sanity";
+import { BookIcon } from "@sanity/icons";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [
+    {
+      name: "project",
+      title: "Project",
+      icon: BookIcon,
+      type: "document",
+      fields: [
+        defineField({
+          name: "title",
+          title: "Title",
+          type: "string",
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: "description",
+          title: "Description",
+          type: "string",
+        }),
+        defineField({
+          name: "link",
+          title: "Link",
+          type: "string",
+        }),
+        defineField({
+          name: "tags",
+          title: "Tags",
+          type: "array",
+          // @ts-ignore
+          of: [{ type: "string" }],
+        }),
+      ],
+    },
+    {
+      name: "experience",
+      title: "Experience",
+      icon: BookIcon,
+      type: "document",
+      fields: [
+        defineField({
+          name: "title",
+          title: "Title",
+          type: "string",
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: "description",
+          title: "Description",
+          type: "string",
+        }),
+        defineField({
+          name: "location",
+          title: "location",
+          type: "string",
+        }),
+        defineField({
+          name: "links",
+          title: "Link",
+          type: "array",
+          // @ts-ignore
+          of: [{ type: "string" }],
+        }),
+        defineField({
+          name: "date",
+          title: "Date",
+          type: "string",
+        }),
+        defineField({
+          name: "skills",
+          title: "Skills",
+          type: "array",
+          // @ts-ignore
+          of: [{ type: "string" }],
+        }),
+        defineField({
+          name: "tags",
+          title: "Tags",
+          type: "array",
+          // @ts-ignore
+          of: [{ type: "string" }],
+        }),
+      ],
+    },
+  ],
+};
