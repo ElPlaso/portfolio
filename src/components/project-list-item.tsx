9fd9696820ad60a4510f55b35e03e3696b711900
ElPlaso <plahsoekusay@gmail.com>
2023-09-08 16:08:29 +1200

    only show visit button if href exists.

--- src/components/project-list-item.tsx
+++ src/components/project-list-item.tsx
@@ -22,32 +22,35 @@ export default function ProjectListItem({ project }: { project: Project }) {
       {project.badges.map((badge) => (
         <Badge key={badge} label={badge} />
       ))}
-      <p className="text-base mb-4 mt-2 font-normal text-gray-500 dark:text-gray-400 max-sm:text-sm">
+      <p className="text-base mt-2 font-normal text-gray-500 dark:text-gray-400 max-sm:text-sm">
         {project.description}
       </p>
-      <a
-        href={project.href}
-        target="_blank"
-        className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700 max-sm:text-xs"
-      >
-        Visit{" "}
-        <svg
-          className="w-3 h-3 ml-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
+
+      {project.href && (
+        <a
+          href={project.href}
+          target="_blank"
+          className="inline-flex items-center mt-4 px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700 max-sm:text-xs"
         >
-          {" "}
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          />
-        </svg>
-      </a>
+          Visit{" "}
+          <svg
+            className="w-3 h-3 ml-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            {" "}
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      )}
     </TimeLineCard>
   );
 }
