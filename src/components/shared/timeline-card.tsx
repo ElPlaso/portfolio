ca784090cd2b3c2aecab3bae871617bf7aa6fc17
ElPlaso <plahsoekusay@gmail.com>
2023-09-08 18:08:34 +1200

    update snapping to better suit mobile devices.

--- src/components/shared/timeline-card.tsx
+++ src/components/shared/timeline-card.tsx
@@ -4,7 +4,7 @@ export default function TimeLineCard({
   children: React.ReactNode;
 }) {
   return (
-    <div className="px-4 pt-2 pb-4 shadow-lg rounded-2xl border-[1px] dark:border-none dark:bg-[rgb(18,18,18)] dark:shadow-2xl max-sm:snap-center max-sm:shrink-0">
+    <div className="px-4 pt-2 pb-4 shadow-lg rounded-2xl border-[1px] dark:border-none dark:bg-[rgb(18,18,18)] dark:shadow-2xl max-sm:snap-end max-sm:shrink-0">
       <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
       {children}
     </div>
