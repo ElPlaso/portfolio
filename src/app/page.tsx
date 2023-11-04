75361a43b62fde813eb7661dab6c354f70e17560
kusaymoo <kusaymoo@ecs.vuw.ac.nz>
2023-11-04 21:19:21 +1200

    decrease padding between arrow and welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="flex flex-col p-4 space-y-8 w-[700px] mt-8 max-md:mt-0 max-md:w-[100%] items-start justify-start mb-16">
+      <div className="flex flex-col p-4 space-y-4 w-[700px] mt-8 max-md:mt-0 max-md:w-[100%] items-start justify-start mb-16">
         <div className="w-full flex flex-row justify-end">
           <Link href="/portfolio">
             <svg
