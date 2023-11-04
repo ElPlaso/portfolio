5ca9b97213579b05bfcd4f471c2a1e5c183b0aff
kusaymoo <kusaymoo@ecs.vuw.ac.nz>
2023-11-04 22:32:30 +1200

    update padding in welcome component

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -11,37 +11,36 @@ import Banner from "./banner";
 export default function Welcome() {
   return (
     <DetailsProvider>
-      <div className="space-y-8 w-full">
-        <div className="space-y-16">
-          <AnimationWrapper>
-            <Banner />
-          </AnimationWrapper>
-          <div className="space-y-4">
-            <h1 className="text-black dark:text-grayDark text-2xl font-medium">
-              Welcome to my website
-            </h1>
-            <h3>
-              <span className="font-bold">plasokusay</span> is able to connect.
-            </h3>
-            <div>
-              Try:
-              <ul className="list-disc ml-10">
-                <li>Checking my contact info</li>
-                <li>
-                  <Link
-                    className="text-blue-500 dark:text-blueDark hover:underline"
-                    href="/portfolio"
-                  >
-                    Checking out my portfolio
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <p className="text-xs">SUCC_CONNECTION_ACCEPTED</p>
+      <div className="space-y-16 w-full">
+        <AnimationWrapper>
+          <Banner />
+        </AnimationWrapper>
+        <div className="space-y-4">
+          <h1 className="text-black dark:text-grayDark text-2xl font-medium">
+            Welcome to my website
+          </h1>
+          <h3>
+            <span className="font-bold">plasokusay</span> is able to connect.
+          </h3>
+          <div>
+            Try:
+            <ul className="list-disc ml-10">
+              <li>Checking my contact info</li>
+              <li>
+                <Link
+                  className="text-blue-500 dark:text-blueDark hover:underline"
+                  href="/portfolio"
+                >
+                  Checking out my portfolio
+                </Link>
+              </li>
+            </ul>
           </div>
+          <p className="text-xs">SUCC_CONNECTION_ACCEPTED</p>
         </div>
-        <div
-          className={`w-full flex flex-row
+        <div className="space-y-8">
+          <div
+            className={`w-full flex flex-row
             justify-between max-sm:flex-col-reverse
             max-sm:space-y-4 max-md:fixed max-md:bottom-0
             max-md:pb-4 max-md:left-0 max-md:px-4 
@@ -49,11 +48,12 @@ export default function Welcome() {
             max-md:to-transparent max-md:via-white
             dark:max-md:via-darkBackground dark:max-md:from-darkBackground
             dark:max-md:to-transparent max-md:z-40`}
-        >
-          <DetailsButton />
-          <ReloadButton />
+          >
+            <DetailsButton />
+            <ReloadButton />
+          </div>
+          <Details />
         </div>
-        <Details />
       </div>
     </DetailsProvider>
   );
